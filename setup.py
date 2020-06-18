1de8e9aec7bd5a86fb2f368805de1a3d63a285fe
Giacomo Marchioro <marchiorogiacomo@gmail.com>
2020-06-18 17:40:30 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="micropro",
-    version="0.0.11",
+    version="0.1.0",
     author="Giacomo Marchioro",
     author_email="giacomomarchioro@outlook.com",
     description="A tool for working with microprofilometry data.",
