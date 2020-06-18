d1e238762d5fbb2e8c2e328b2b042fbedc7f31a9
Giacomo Marchioro <marchiorogiacomo@gmail.com>
2020-06-18 17:09:55 +0200

    Changed config method

--- micropro/config.py
+++ micropro/config.py
@@ -6,12 +6,13 @@ print(configfile)
 if hasattr(__builtins__, 'raw_input'):
     input = raw_input
 
+if os.path.exists(configfile):
+    with open(configfile, 'r') as f:
+        myconfig = json.load(f)
 
-with open(configfile, 'r') as f:
-    myconfig = json.load(f)
-
-if myconfig['databasepath'] is None:
-    myconfig['databasepath'] = input(("Specify the database"
-    " path if LabDB is installed in the system: "))
+else:
     with open(configfile, 'wb') as f:
+        myconfig = {}
+        myconfig['databasepath'] = input(("Specify the database"
+    " path if LabDB is installed in the system: "))
         json.dump(myconfig, f)
