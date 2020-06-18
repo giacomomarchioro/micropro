ff7ca14a13bbba99609765d291d0273169ecc2d2
Giacomo Marchioro <marchiorogiacomo@gmail.com>
2020-06-18 15:57:50 +0200

    Updated setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="micropro",
-    version="0.0.6",
+    version="0.0.7",
     author="Giacomo Marchioro",
     author_email="giacomomarchioro@outlook.com",
     description="A tool for working with microprofilometry data.",
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/giacomomarchioro/micropro",
     packages=setuptools.find_packages(),
+    data_files=[('config', ['config.json'])],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
